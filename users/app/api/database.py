8729cd794354ed9532184ba8518c8ea6e70d404f
biggvladek <zuevvlad55@gmail.com>
2022-08-06 16:18:18 +0300

    database_methods

--- users/app/api/database.py
+++ users/app/api/database.py
@@ -5,39 +5,79 @@ class Database():
     def __init__(self):
         self.client = MongoClient()
         self.database = self.client.users
-        self.users = self.database.users
+        self.collection = self.database.users
 
 
     def add_user(self,nickname,password,country,city):
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        nicknames = self.get_all_nickname()
+        if nickname not in nicknames:
 
-        user = {
-            'nickname': nickname,
-            'password': hashed_password,
-            'country': country,
-            'city': city,
-        }
+            user = {
+                'nickname': nickname,
+                'password': hashed_password,
+                'country': country,
+                'city': city,
+            }
 
-        self.users.insert_one(user)
+            self.collection.insert_one(user)
 
-
-
-    def change_password(self):
-        pass
-
-
-
-    def change_country(self):
-        pass
+        else:
+            return 'Error'
 
 
-    def change_city(self):
-        pass
 
-    
-    def delete_account(self):
+    def change_password(self,nickname,password,new_password):
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        new_hashed_password = hashlib.sha256(new_password.encode('utf-8')).hexdigest()
 
-        pass
 
-a= Database()
+        self.collection.update_one({'nickname':nickname,'password':hashed_password},
+                                   {'$set':
+                                        {'password':new_hashed_password}
+                                    }
+                                )
+
+
+
+
+    def change_country(self,new_country,password,nickname):
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        self.collection.update_one({'nickname': nickname, 'password': hashed_password},
+                              {'$set':
+                                   {'country': new_country}
+                               }
+                              )
+
+
+
+    def change_city(self,new_city,password,nickname):
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        self.collection.update_one({'nickname': nickname, 'password': hashed_password},
+                              {'$set':
+                                   {'city': new_city}
+                               }
+                              )
+
+
+    def delete_account(self,nickname,password):
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        self.collection.delete_one({'nickname':nickname, 'password':hashed_password})
+
+
+    def get_all_nickname(self) -> list:
+        A = []
+        res = self.collection.find({}, {'_id': 0, 'password': 0, 'country': 0,'city':0})
+        for i in res:
+            A.append(i['nickname'])
+        return A
+
+
+
+
+
+
 
